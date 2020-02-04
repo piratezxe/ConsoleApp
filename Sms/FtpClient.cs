2a06c2aefc1fd78a3de08f0edcf3395388b3385e
Carlos <karol.pisarzewski@gmail.com>
2020-02-04 07:51:03 +0100

    initial

---
+++ Sms/FtpClient.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Net;
+
+namespace Sms
+{
+    public class FtpClient : IFtpClient
+    {
+        public string HostName { get; private set; }
+
+        public string UserName { get; private set; }
+
+        public string Password { get; private set; }
+
+        public FtpClient(string hostName, string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+            HostName = hostName;
+        }
+
+
+        public void Download(string file, string destination)
+        {
+         
+            FtpWebRequest request =
+                (FtpWebRequest)WebRequest.Create(HostName + file);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials =
+                new NetworkCredential(UserName, Password);
+            FtpWebResponse response =
+                (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+
+            StreamWriter writer = new StreamWriter(destination);
+            writer.Write(reader.ReadToEnd());
+
+            writer.Close();
+            reader.Close();
+            response.Close();
+        }
+    }
+    public class DiscClient : IFtpClient
+    {
+        public void Download(string file, string destination)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(destination));
+            File.Copy(file, destination);
+        }
+    }
+}
---
+++ Sms/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFtpClient ftpClient = new DiscClient();
+            IImportService importService = new DiscImportService(ftpClient, "C:\\Users\\Karol\\Desktop\\ftp", "C:\\Users\\Karol\\Desktop\\local");
+            importService.ImportRecord("C:\\Users\\Karol\\Desktop\\ftp", "C:\\Users\\Karol\\Desktop\\local");
+        }
+    }
+}
